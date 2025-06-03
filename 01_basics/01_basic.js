c5573ada724356faf75b0c46dcc15c4996630880
khuranaharshit123 <khuranaharshit40@gmail.com>
2025-06-03 13:49:38 +0000

    container config

---
+++ 01_basics/01_basic.js
@@ -0,0 +1,13 @@
+const password=1223
+let id="hars"
+var name="hars"
+StateName="jaipur"
+let state
+
+// do not use var in java script
+
+id="hum"
+name="hum"
+StateName="banglore"
+console.log(name)
+console.table([id,name,StateName,password,state])
